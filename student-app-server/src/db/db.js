1e01c64e1d34643ad7491c51662811c482167654
ReddybashaShaik <reddybasha8142@gmail.com>
2021-12-14 18:55:12 +0530

    db password

--- student-app-server/src/db/db.js
+++ student-app-server/src/db/db.js
@@ -4,7 +4,7 @@ export const getConnection = () => {
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: '1234',
+        password: '12345',
         database: 'student'
     });;
 }
